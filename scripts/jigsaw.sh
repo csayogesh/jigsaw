e3f4650d4d51091555b1db7b67e1029304e115b3
Yogesh Bhosale <ybhosale12@gmail.com>
2015-10-05 15:06:06 +0530

    Able to pick the correct classes now.

--- scripts/jigsaw.sh
+++ scripts/jigsaw.sh
@@ -10,8 +10,9 @@ export JIGSAW_HOME
 LD_LIBRARY_PATH=${JIGSAW_HOME}/lib
 export LD_LIBRARY_PATH
 
-CLASSPATH=${JIGSAW_HOME}/ant.build/classes/core:${JIGSAW_HOME}/ant.build/classes/admin:${JIGSAW_HOME}/ant.build/classes/edit:${JIGSAW_HOME}/classes/sax.jar:${JIGSAW_HOME}/classes/xp.jar:${JIGSAW_HOME}/classes/servlet.jar:${JIGSAW_HOME}/classes/xerces.jar:${JIGSAW_HOME}/classes/Tidy.jar
+CLASSPATH=${JIGSAW_HOME}/ant.build/classes/core/tmpclasses:${JIGSAW_HOME}/ant.build/classes/admin:${JIGSAW_HOME}/ant.build/classes/edit:${JIGSAW_HOME}/classes/sax.jar:${JIGSAW_HOME}/classes/xp.jar:${JIGSAW_HOME}/classes/servlet.jar:${JIGSAW_HOME}/classes/xerces.jar:${JIGSAW_HOME}/classes/Tidy.jar:${JIGSAW_HOME}/../Fastrack/classes
 export CLASSPATH
 
 java -Xms16m -Xmx128m org.w3c.jigsaw.Main -root ${JIGSAW_HOME}/Jigsaw $* 
 # done.
+
